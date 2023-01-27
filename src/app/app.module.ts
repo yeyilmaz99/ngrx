a8d4cdb299da2f0c6bf271f000f276886e4b3126
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-01-27 12:51:17 +0300

    7. video

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,22 @@ import { CounterOutputComponent } from './counter/counter-output/counter-output.
 import { CounterButtonsComponent } from './counter/counter-buttons/counter-buttons.component';
 import { StoreModule } from '@ngrx/store';
 import { counterReducer } from './counter/state/counter.reducer';
+import { CustomCounterInputComponent } from './counter/custom-counter-input/custom-counter-input.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     CounterComponent,
     CounterOutputComponent,
-    CounterButtonsComponent
+    CounterButtonsComponent,
+    CustomCounterInputComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({counter: counterReducer})
+    StoreModule.forRoot({counter: counterReducer}),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
