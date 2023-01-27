7cd6181bf789589f66b868df0ed749fe654a001f
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-01-27 12:51:23 +0300

    7. video

--- src/app/counter/counter-output/counter-output.component.ts
+++ src/app/counter/counter-output/counter-output.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Observable, Subscription } from 'rxjs';
+import { CounterState } from '../state/counter.state';
 
 @Component({
   selector: 'app-counter-output',
@@ -7,13 +9,14 @@ import { Store } from '@ngrx/store';
   styleUrls: ['./counter-output.component.css']
 })
 export class CounterOutputComponent implements OnInit {
-  constructor(private store: Store< {counter : {counter:number}}>) { }
+  constructor(private store: Store< {counter : CounterState}>) { }
   counter:number;
+  counter$: Observable<{counter:number}>
 
   ngOnInit(): void {
-    this.store.select('counter').subscribe(data => {
-      this.counter = data.counter;
-    })
+    this.counter$ = this.store.select('counter');
   }
+
+
 
 }
