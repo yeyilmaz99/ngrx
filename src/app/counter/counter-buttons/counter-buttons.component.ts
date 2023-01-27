aae6b1e40526b81fc19aa3df6c45bba4d7e7be44
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-01-27 08:38:09 +0300

    functions

--- src/app/counter/counter-buttons/counter-buttons.component.ts
+++ src/app/counter/counter-buttons/counter-buttons.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { decrement, increment, reset } from '../state/counter.actions';
 
 @Component({
   selector: 'app-counter-buttons',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CounterButtonsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private store:Store<{counter : {counter: number}}>) { }
 
   ngOnInit(): void {
   }
+
+  onIncrement(){
+    this.store.dispatch(increment());
+  };
+
+  onDecrement(){
+    this.store.dispatch(decrement());
+  };
+  onReset(){
+    this.store.dispatch(reset());
+  };
 
 }
