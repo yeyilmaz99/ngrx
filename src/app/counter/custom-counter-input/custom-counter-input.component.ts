0d367ec778fe6484ded7448189cdf567fcb9240e
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-01-31 14:24:15 +0300

    update

--- src/app/counter/custom-counter-input/custom-counter-input.component.ts
+++ src/app/counter/custom-counter-input/custom-counter-input.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { AppState } from 'src/app/store/app.state';
 import { changeText, customIncrement } from '../state/counter.actions';
 import { getChannelText } from '../state/counter.selectors';
 import { CounterState } from '../state/counter.state';
@@ -16,7 +17,7 @@ export class CustomCounterInputComponent implements OnInit {
   channelName$:Observable<string>;
 
 
-  constructor(private store:Store<{counter : CounterState}>) { }
+  constructor(private store:Store<AppState>) { }
 
   ngOnInit(): void {
     this.channelName$ = this.store.select(getChannelText);
