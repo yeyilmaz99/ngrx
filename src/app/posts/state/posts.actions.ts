2a7a15d0bd74b4313f5ed0a9425e7faa1111f20f
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-02-02 16:10:27 +0300

    postActions

--- src/app/posts/state/posts.actions.ts
+++ src/app/posts/state/posts.actions.ts
@@ -1,2 +1,7 @@
+import { createAction, props } from "@ngrx/store";
+import { Post } from "src/app/models/post.model";
 
 
+export const ADD_POST_ACTION = '[post page] add post';
+
+export const addPost = createAction(ADD_POST_ACTION, props<{post:Post}>())
