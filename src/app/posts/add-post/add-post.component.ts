eb6d3d080ce3405ad4d7867baf9867c2482e1354
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-02-02 16:10:13 +0300

    add-post

--- src/app/posts/add-post/add-post.component.ts
+++ src/app/posts/add-post/add-post.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { Post } from 'src/app/models/post.model';
+import { AppState } from 'src/app/store/app.state';
+import { addPost } from '../state/posts.actions';
 
 @Component({
   selector: 'app-add-post',
@@ -11,7 +15,7 @@ export class AddPostComponent implements OnInit {
 
   postForm: FormGroup;
 
-  constructor() { }
+  constructor(private store:Store<AppState>) { }
 
   ngOnInit(): void {
     this.postForm = new FormGroup({
@@ -21,7 +25,15 @@ export class AddPostComponent implements OnInit {
   }
 
   onAddPost() {
-    console.log(this.postForm.value);
+    if(!this.postForm.valid){
+      return;
+    }
+
+    const post:Post = {
+      title: this.postForm.value.title,
+      description: this.postForm.value.description
+    }
+    this.store.dispatch(addPost({post}))
   }
 
   showDescriptionErrors() {
