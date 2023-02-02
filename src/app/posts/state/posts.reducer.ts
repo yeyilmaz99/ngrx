1cac15d2ebc16d56ea925d02431419d6e18312aa
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-02-02 16:10:34 +0300

    postReducer

--- src/app/posts/state/posts.reducer.ts
+++ src/app/posts/state/posts.reducer.ts
@@ -1,7 +1,15 @@
-import { createReducer } from "@ngrx/store";
+import { createReducer, on } from "@ngrx/store";
+import { addPost } from "./posts.actions";
 import { initialState } from "./posts.state";
 
-const _postReducer = createReducer(initialState)
+const _postReducer = createReducer(initialState, on(addPost, (state,action) =>{
+    let post = {...action.post};
+    post.id = (state.posts.length+1).toString();
+    return {
+        ...state,
+        posts: [...state.posts, post]
+    }
+}))
 
 
 export function postsReducer(state:any, action:any){
