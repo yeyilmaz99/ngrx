fcda84779e504718b478d6dbad39f3a4142cf58a
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-02-02 18:31:57 +0300

    post Selector getpostbyid

--- src/app/posts/state/posts.selector.ts
+++ src/app/posts/state/posts.selector.ts
@@ -8,3 +8,7 @@ const getPostsState = createFeatureSelector<PostsState>('posts');
 export const getPosts = createSelector(getPostsState, (state) => {
     return state.posts;
 })
+
+export const getPostById = createSelector(getPostsState, (state:any, props:any) => {
+    return state.posts.find((post:any) => post.id === props.id)
+})
