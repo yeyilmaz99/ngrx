ae349f1e899234ee4f0870bfbf7a767152bccda1
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-02-02 23:09:40 +0300

    post actions

--- src/app/posts/state/posts.actions.ts
+++ src/app/posts/state/posts.actions.ts
@@ -4,4 +4,10 @@ import { Post } from "src/app/models/post.model";
 
 export const ADD_POST_ACTION = '[post page] add post';
 
-export const addPost = createAction(ADD_POST_ACTION, props<{post:Post}>())
+export const UPDATE_POST_ACTION = '[posts page] update post';
+
+export const addPost = createAction(ADD_POST_ACTION, props<{post:Post}>());
+
+export const updatePost = createAction(UPDATE_POST_ACTION, props<{post:Post}>())
+
+
