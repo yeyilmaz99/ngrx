cf227df6f5be51f759305f6d8db17b7b90c66a3f
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-02-02 18:30:52 +0300

    editpost

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { environment } from 'src/environments/environment';
 import { postsReducer } from './posts/state/posts.reducer';
 import { appReducer } from './store/app.state';
 import { AddPostComponent } from './posts/add-post/add-post.component';
+import { EditPostComponent } from './posts/edit-post/edit-post.component';
 
 @NgModule({
   declarations: [
@@ -30,6 +31,7 @@ import { AddPostComponent } from './posts/add-post/add-post.component';
     HeaderComponent,
     PostListComponent,
     AddPostComponent,
+    EditPostComponent,
   ],
   imports: [
     BrowserModule,
