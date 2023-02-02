290dfa84181e551f3cf4585535da332f08d91764
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-02-02 23:20:05 +0300

    delete operation

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -3,6 +3,7 @@ import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { Post } from 'src/app/models/post.model';
 import { AppState } from 'src/app/store/app.state';
+import { deletePost } from '../state/posts.actions';
 import { getPosts } from '../state/posts.selector';
 import { PostsState } from '../state/posts.state';
 
@@ -17,6 +18,13 @@ export class PostListComponent implements OnInit {
 
   ngOnInit(): void {
     this.posts = this.store.select(getPosts);
+  }
+
+
+  onDeletePost(id:string){
+    if(confirm('are you sure')){
+      this.store.dispatch(deletePost({id}))
+    }
   }
 
 }
