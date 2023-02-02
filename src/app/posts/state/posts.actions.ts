52dee6457cf48466b98c62b276e812019c03228e
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-02-02 23:20:08 +0300

    delete operation

--- src/app/posts/state/posts.actions.ts
+++ src/app/posts/state/posts.actions.ts
@@ -6,8 +6,12 @@ export const ADD_POST_ACTION = '[post page] add post';
 
 export const UPDATE_POST_ACTION = '[posts page] update post';
 
+export const DELETE_POST_ACTION = '[posts page] delete post';
+
+
 export const addPost = createAction(ADD_POST_ACTION, props<{post:Post}>());
 
 export const updatePost = createAction(UPDATE_POST_ACTION, props<{post:Post}>())
 
+export const deletePost = createAction(DELETE_POST_ACTION, props<{id:string}>())
 
