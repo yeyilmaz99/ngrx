9544f1c54c69dc12a5442a9b0545b12225b7a947
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-02-02 23:09:30 +0300

    editPost

--- src/app/posts/edit-post/edit-post.component.ts
+++ src/app/posts/edit-post/edit-post.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { State, Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
 import { Post } from 'src/app/models/post.model';
 import { AppState } from 'src/app/store/app.state';
+import { updatePost } from '../state/posts.actions';
 import { getPostById } from '../state/posts.selector';
 
 @Component({
@@ -17,7 +18,9 @@ export class EditPostComponent implements OnInit, OnDestroy {
   postForm:FormGroup;
   postSubscription: Subscription
   constructor(private route:ActivatedRoute,
-     private store :Store<AppState>) { }
+     private store :Store<AppState>,
+     private router:Router
+     ) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
@@ -44,6 +47,46 @@ export class EditPostComponent implements OnInit, OnDestroy {
 
   }
 
-  
+
+  showDescriptionErrors() {
+    const descriptionForm = this.postForm.get('description');
+    if (descriptionForm.touched && !descriptionForm.valid) {
+      if (descriptionForm.errors.required) {
+        return 'Description is required'
+      }
+      if (descriptionForm.errors.minlength) {
+        return 'description should be of min 10 characters'
+      }
+    }
+  }
+
+  showTitleErrors(){
+    const titleForm = this.postForm.get('title');
+    if(titleForm.touched && !titleForm.valid){
+      if(titleForm.errors.required){
+        return 'Title is required';
+      }
+
+      if(titleForm.errors.minlength){
+        return 'Title is must be min 6 characters';
+      }
+    }
+  }
+
+  onSubmit(){
+    if(!this.postForm.valid){
+      return;
+    }
+    const title = this.postForm.value.title;
+    const description = this.postForm.value.description;
+    const post : Post = {
+      id: this.post.id,
+      title, description
+    }
+    this.store.dispatch(updatePost({post}));
+    this.router.navigate(['posts'])
+  }
+
+
 
 }
