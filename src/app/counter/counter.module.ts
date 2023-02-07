217921b4e135b2c162ebd77de5e20b9501d56f5d
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-02-07 22:15:11 +0300

    lazyloading

---
+++ src/app/counter/counter.module.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterModule, Routes } from "@angular/router";
+import { CounterButtonsComponent } from "./counter-buttons/counter-buttons.component";
+import { CounterOutputComponent } from "./counter-output/counter-output.component";
+import { CounterComponent } from "./counter/counter.component";
+import { CustomCounterInputComponent } from "./custom-counter-input/custom-counter-input.component";
+
+
+
+
+const routes: Routes = [
+    {
+        path: '', component: CounterComponent
+    },
+]
+@NgModule({
+    declarations: [
+        CounterComponent,
+        CounterOutputComponent,
+        CounterButtonsComponent,
+        CustomCounterInputComponent,
+    ],
+    imports: [
+        CommonModule,
+        RouterModule.forChild(routes),
+        FormsModule
+    ],
+        
+})
+export class CounterModule {
+
+}
