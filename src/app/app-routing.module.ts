cd6e384445ea97dd4260bf5e8ced21e400d5534c
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-02-07 22:15:03 +0300

    lazyloadin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CounterComponent } from './counter/counter/counter.component';
 import { HomeComponent } from './home/home.component';
-import { AddPostComponent } from './posts/add-post/add-post.component';
-import { EditPostComponent } from './posts/edit-post/edit-post.component';
-import { PostListComponent } from './posts/post-list/post-list.component';
 
 const routes: Routes = [
   {
     path: '', component:HomeComponent
   },
   {
-    path:'counter', component:CounterComponent
+    path:'counter', loadChildren: () => import('./counter/counter.module').then(m => m.CounterModule)
   },
   {
-    path:'posts', component:PostListComponent,
-    children: [
-      {path: 'add', component:AddPostComponent},
-      {path:'edit/:id', component:EditPostComponent}
-    ]
+    path:'posts', loadChildren:() => import('./posts/posts.module').then(m => m.PostsModule)
   }
 ];
 
