9eb01503956c9310e58d77e91855d1b72dff77b7
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-02-16 22:19:02 +0300

    lazyload store module forFeature

--- src/app/counter/counter.module.ts
+++ src/app/counter/counter.module.ts
@@ -2,10 +2,13 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { RouterModule, Routes } from "@angular/router";
+import { StoreModule } from "@ngrx/store";
 import { CounterButtonsComponent } from "./counter-buttons/counter-buttons.component";
 import { CounterOutputComponent } from "./counter-output/counter-output.component";
 import { CounterComponent } from "./counter/counter.component";
 import { CustomCounterInputComponent } from "./custom-counter-input/custom-counter-input.component";
+import { counterReducer } from "./state/counter.reducer";
+import { COUNTER_SELECTOR_NAME } from "./state/counter.selectors";
 
 
 
@@ -25,7 +28,8 @@ const routes: Routes = [
     imports: [
         CommonModule,
         RouterModule.forChild(routes),
-        FormsModule
+        FormsModule,
+        StoreModule.forFeature(COUNTER_SELECTOR_NAME, counterReducer)
     ],
         
 })
