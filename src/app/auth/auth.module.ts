53d342f1c83584354c23e79e5f09751ec78968a2
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-02-25 22:33:19 +0300

    auth effects

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,8 +2,10 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 import { RouterModule, Routes } from "@angular/router";
+import { EffectsModule } from "@ngrx/effects";
 import { StoreModule } from "@ngrx/store";
 import { LoginComponent } from "./login/login.component";
+import { AuthEffects } from "./state/auth.effects";
 import { AuthReducer } from "./state/auth.reducer";
 import { AUTH_STATE_NAME } from "./state/auth.selector";
 
@@ -29,7 +31,8 @@ const routes: Routes = [
         CommonModule,
         RouterModule.forChild(routes),
         ReactiveFormsModule,
-        StoreModule.forFeature(AUTH_STATE_NAME,AuthReducer)
+        StoreModule.forFeature(AUTH_STATE_NAME,AuthReducer),
+        EffectsModule.forFeature([AuthEffects])
     ],
 
 })
