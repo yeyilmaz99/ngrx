9c14305ff283a53b287a1f2bc4bce58bd088cfe1
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-02-25 19:50:59 +0300

    auth

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterModule, Routes } from "@angular/router";
+import { LoginComponent } from "./login/login.component";
+
+
+
+const routes: Routes = [
+    {
+        path: '', children: [
+            { path: '', pathMatch:"full", redirectTo: 'login', },
+            {
+                path: 'login', component: LoginComponent
+            }
+        ]
+    }
+]
+
+
+
+@NgModule({
+
+    declarations: [LoginComponent],
+    imports: [
+        CommonModule,
+        RouterModule.forChild(routes),
+        ReactiveFormsModule,
+    ],
+
+})
+
+export class AuthModule {
+
+}
