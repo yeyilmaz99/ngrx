4adce52435d7e6b74a32e81ca7e6ae7592b04f98
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-02-27 02:10:57 +0300

    ErrorHandling

--- src/app/auth/state/auth.effects.ts
+++ src/app/auth/state/auth.effects.ts
@@ -1,10 +1,10 @@
 import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { Store } from "@ngrx/store";
-import { exhaustMap, map } from "rxjs";
+import { catchError, exhaustMap, map, of } from "rxjs";
 import { AuthService } from "src/app/services/auth.service";
 import { AppState } from "src/app/store/app.state";
-import { setLoadingSpinner } from "src/app/store/shared/shared.actions";
+import { setErrorMessage, setLoadingSpinner } from "src/app/store/shared/shared.actions";
 import { loginStart, loginSuccess } from "./auth.actions";
 
 
@@ -22,9 +22,15 @@ export class AuthEffects {
             return this.authService.login(action.email, action.password).pipe(
               map((data) => {
                 this.store.dispatch(setLoadingSpinner({status:false}));
+                this.store.dispatch(setErrorMessage({message:''}))
                 const user = this.authService.formatUser(data);
                 return loginSuccess({user});
               }),
+              catchError((errResp) => {
+                const errorMessage = this.authService.getErrorMessage(errResp.error.error.message);
+                this.store.dispatch(setLoadingSpinner({status:false}));
+                return of(setErrorMessage({message: errorMessage}));
+              })
             );
           })
         );
