a9420ae1ff78e1da8faadf249e92dd92aff9c13d
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-02-27 02:11:01 +0300

    ErrorHandling

--- src/app/store/shared/shared.reducer.ts
+++ src/app/store/shared/shared.reducer.ts
@@ -1,15 +1,21 @@
 import { createReducer, on } from "@ngrx/store";
-import { setLoadingSpinner } from "./shared.actions";
+import { setErrorMessage, setLoadingSpinner } from "./shared.actions";
 import { initialState } from "./shared.state";
 
 
 
 const _sharedReducer = createReducer(initialState, on(setLoadingSpinner,(state,action) => {
     return {
-        state,
+        ...state,
         showLoading: action.status,
     };
-}))
+}), on(setErrorMessage, (state,action) => {
+    return {
+        ...state,
+        errorMessage: action.message
+    }
+})
+)
 
 
 export function SharedReducer (state:any, action:any){
