ecd5c295e99d9c35061532a903afae8cf9648f0c
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-02-26 21:39:59 +0300

    auth

--- src/app/auth/state/auth.effects.ts
+++ src/app/auth/state/auth.effects.ts
@@ -1,14 +1,17 @@
 import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { Store } from "@ngrx/store";
 import { exhaustMap, map } from "rxjs";
 import { AuthService } from "src/app/services/auth.service";
+import { AppState } from "src/app/store/app.state";
+import { setLoadingSpinner } from "src/app/store/shared/shared.actions";
 import { loginStart, loginSuccess } from "./auth.actions";
 
 
 
 @Injectable()
 export class AuthEffects {
-    constructor(private actions$: Actions, private authService: AuthService) {
+    constructor(private actions$: Actions, private authService: AuthService, private store:Store<AppState>) {
 
     }
 
@@ -18,7 +21,9 @@ export class AuthEffects {
           exhaustMap(action => {
             return this.authService.login(action.email, action.password).pipe(
               map((data) => {
-                return loginSuccess();
+                this.store.dispatch(setLoadingSpinner({status:false}));
+                const user = this.authService.formatUser(data);
+                return loginSuccess({user});
               }),
             );
           })
