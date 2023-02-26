6c51a8db04779e1e4cffd11e46751a12102ab7c6
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-02-27 02:10:56 +0300

    ErrorHandling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { AppState } from './store/app.state';
-import { getLoading } from './store/shared/shared.selector';
+import { getErrorMessage, getLoading } from './store/shared/shared.selector';
 
 @Component({
   selector: 'app-root',
@@ -12,11 +12,13 @@ import { getLoading } from './store/shared/shared.selector';
 export class AppComponent implements OnInit {
   title = 'ngrx';
   showLoading:Observable<boolean>;
+  errorMessage:Observable<string>;
 
   constructor(private store:Store<AppState>){};
 
   ngOnInit(): void {
     this.showLoading = this.store.select(getLoading);
+    this.errorMessage = this.store.select(getErrorMessage);
 
   }
 }
