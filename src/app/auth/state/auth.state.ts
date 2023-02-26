ba89d43ad0ec17c626f663bd3756e13d6a8d363d
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-02-26 21:40:03 +0300

    auth

--- src/app/auth/state/auth.state.ts
+++ src/app/auth/state/auth.state.ts
@@ -1,7 +1,10 @@
+import { User } from "src/app/models/user.model"
 
 
 export interface AuthState {
-
+    user:User | null
 }
 
-export const initialState:AuthState = {}
+export const initialState:AuthState = {
+    user:null,
+}
