273024242e147b135a162467069d156fefea3624
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-02-26 21:40:08 +0300

    auth

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,22 +3,34 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
 import { AuthResponseData } from "../models/authResponseData.model";
+import { User } from "../models/user.model";
 
 
 @Injectable({
-    providedIn:'root'
+    providedIn: 'root'
 })
 
 export class AuthService {
-    constructor(private http:HttpClient){
+    constructor(private http: HttpClient) {
 
     }
 
-    login(email: string, password: string):Observable<AuthResponseData> {
+    login(email: string, password: string): Observable<AuthResponseData> {
         return this.http.post<AuthResponseData>(
-          `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.FIREBASE_API_KEY}`,
-          { email, password, returnSecureToken: true }
+            `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.FIREBASE_API_KEY}`,
+            { email, password, returnSecureToken: true }
         );
-      }
+    }
+
+    formatUser(data:AuthResponseData){
+        const expirationDate = new Date(
+            new Date().getTime() + +data.expiresIn * 1000
+          );
+        console.log(expirationDate)
+        const user = new User(data.email, data.idToken, data.localId,expirationDate)
+        return user;
+    }
+
+
 
 }
