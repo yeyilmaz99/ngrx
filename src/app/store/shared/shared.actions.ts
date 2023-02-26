6082cceca367a73eb8b02e6b979ee9ff39e54363
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-02-26 21:42:25 +0300

    sharedLoadingSpinner

---
+++ src/app/store/shared/shared.actions.ts
@@ -0,0 +1,6 @@
+import { createAction, props } from "@ngrx/store";
+
+export const SET_LOADING_ACTION = '[shared state] set loading spinner';
+
+
+export const setLoadingSpinner = createAction(SET_LOADING_ACTION, props<{status:boolean}>())
