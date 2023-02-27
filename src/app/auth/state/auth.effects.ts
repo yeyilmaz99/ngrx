e774f40d5a0c7f14c998e5b9fe0180c71fab79c3
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-02-27 03:48:04 +0300

    routing

--- src/app/auth/state/auth.effects.ts
+++ src/app/auth/state/auth.effects.ts
@@ -1,7 +1,8 @@
 import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { Store } from "@ngrx/store";
-import { catchError, exhaustMap, map, of } from "rxjs";
+import { catchError, exhaustMap, map, of, tap } from "rxjs";
 import { AuthService } from "src/app/services/auth.service";
 import { AppState } from "src/app/store/app.state";
 import { setErrorMessage, setLoadingSpinner } from "src/app/store/shared/shared.actions";
@@ -11,28 +12,48 @@ import { loginStart, loginSuccess } from "./auth.actions";
 
 @Injectable()
 export class AuthEffects {
-    constructor(private actions$: Actions, private authService: AuthService, private store:Store<AppState>) {
+  constructor(
+    private actions$: Actions, 
+    private authService: AuthService, 
+    private store: Store<AppState>,
+    private router:Router
+    
+    ) {
 
-    }
+  }
 
-    login$ = createEffect(() => {
-        return this.actions$.pipe(
-          ofType(loginStart),
-          exhaustMap(action => {
-            return this.authService.login(action.email, action.password).pipe(
-              map((data) => {
-                this.store.dispatch(setLoadingSpinner({status:false}));
-                this.store.dispatch(setErrorMessage({message:''}))
-                const user = this.authService.formatUser(data);
-                return loginSuccess({user});
-              }),
-              catchError((errResp) => {
-                const errorMessage = this.authService.getErrorMessage(errResp.error.error.message);
-                this.store.dispatch(setLoadingSpinner({status:false}));
-                return of(setErrorMessage({message: errorMessage}));
-              })
-            );
+  login$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(loginStart),
+      exhaustMap(action => {
+        return this.authService.login(action.email, action.password).pipe(
+          map((data) => {
+            this.store.dispatch(setLoadingSpinner({ status: false }));
+            this.store.dispatch(setErrorMessage({ message: '' }))
+            const user = this.authService.formatUser(data);
+            return loginSuccess({ user });
+          }),
+          catchError((errResp) => {
+            const errorMessage = this.authService.getErrorMessage(errResp.error.error.message);
+            this.store.dispatch(setLoadingSpinner({ status: false }));
+            return of(setErrorMessage({ message: errorMessage }));
           })
         );
-      });
+      })
+    );
+  });
+
+
+  loginRedirect$ = createEffect(
+    () => {
+      return this.actions$.pipe(
+        ofType(loginSuccess),
+        tap((action)=> {
+          this.router.navigate(['/']);
+        })
+      )
+    },
+    {dispatch: false}
+  )
+
 }
