d5a2529f472f8e2dfdefb501d5269ea1cab9e4c6
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-02-27 03:47:40 +0300

    auth state globally changed

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,11 +3,9 @@ import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 import { RouterModule, Routes } from "@angular/router";
 import { EffectsModule } from "@ngrx/effects";
-import { StoreModule } from "@ngrx/store";
 import { LoginComponent } from "./login/login.component";
 import { AuthEffects } from "./state/auth.effects";
-import { AuthReducer } from "./state/auth.reducer";
-import { AUTH_STATE_NAME } from "./state/auth.selector";
+
 
 
 
@@ -31,7 +29,6 @@ const routes: Routes = [
         CommonModule,
         RouterModule.forChild(routes),
         ReactiveFormsModule,
-        StoreModule.forFeature(AUTH_STATE_NAME,AuthReducer),
         EffectsModule.forFeature([AuthEffects])
     ],
 
