d0227f218650415d3a0a29ad847098bd9ff56013
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-02-27 03:47:47 +0300

    auth state globally changed

--- src/app/store/app.state.ts
+++ src/app/store/app.state.ts
@@ -1,13 +1,18 @@
 
+import { AuthReducer } from "../auth/state/auth.reducer";
+import { AUTH_STATE_NAME } from "../auth/state/auth.selector";
+import { AuthState } from "../auth/state/auth.state";
 import { SharedReducer } from "./shared/shared.reducer";
 import { SHARED_STATE_NAME } from "./shared/shared.selector";
 import { SharedState } from "./shared/shared.state";
 
 
 export interface AppState {
+    [AUTH_STATE_NAME]:AuthState
     [SHARED_STATE_NAME]:SharedState;
 }
 
 export const appReducer = {
-    [SHARED_STATE_NAME]:SharedReducer
+    [SHARED_STATE_NAME]:SharedReducer,
+    [AUTH_STATE_NAME]:AuthReducer
 }
