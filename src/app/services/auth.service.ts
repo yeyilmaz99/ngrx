842dfce015b52db00523af633cdec3dff024e618
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-03-20 23:46:40 +0300

    autoLogin

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -11,6 +11,7 @@ import { User } from "../models/user.model";
 })
 
 export class AuthService {
+    timeoutInterval:any;
     constructor(private http: HttpClient) {
 
     }
@@ -49,6 +50,36 @@ export class AuthService {
             `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.FIREBASE_API_KEY}`,
             { email, password, returnSecureToken: true }
         );
+    }
+
+
+    setUserInLocalStorage(user:User){
+        localStorage.setItem('userData', JSON.stringify(user));
+
+        this.runTimeoutInterval(user);
+    }
+
+    runTimeoutInterval(user: User){
+        const todaysDate = new Date().getTime();
+        const expirationDate = user.expireDate.getTime();
+        const timeInterval = expirationDate - todaysDate;
+
+        this.timeoutInterval = setTimeout(() => {
+            //logout func or get the refresh token
+        },timeInterval)
+    }
+
+    getUserFromLocalStorage(){
+        const userDataString = localStorage.getItem('userData');
+        if(userDataString){
+            const userData = JSON.parse(userDataString);
+            const expirationDate = new Date(userData.expirationDate)
+            const user = new User(userData.email,userData.token,userData.localId,expirationDate)
+            this.runTimeoutInterval(user);
+            return user;
+        }else{
+            return null;
+        }
     }
 
 
