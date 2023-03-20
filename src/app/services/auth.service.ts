b55745e50a0951a12993772ce99affd6b6ddb05c
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-03-20 21:47:24 +0300

    signup

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -37,6 +37,8 @@ export class AuthService {
                 return 'Email not found';
             case 'INVALID_PASSWORD':
                 return 'Invalid Password';
+            case 'EMAIL_EXISTS':
+                return 'Email is already exists'
             default:
                 return 'Unknown Error Occured, Please try again later';
         }
