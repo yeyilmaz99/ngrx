7f94dfa675b392da3820f25e5e8cb09df6d61e76
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-03-20 21:47:23 +0300

    signup

--- src/app/auth/state/auth.reducer.ts
+++ src/app/auth/state/auth.reducer.ts
@@ -1,5 +1,5 @@
 import { createReducer, on } from "@ngrx/store"
-import { loginSuccess } from "./auth.actions"
+import { loginSuccess, signupSuccess } from "./auth.actions"
 import { initialState } from "./auth.state"
 
 
@@ -7,9 +7,14 @@ const _authReducer = createReducer(initialState, on(loginSuccess, (state,action)
     return {
         ...state,
         user:action.user,
-
     }
-}))
+}),on(signupSuccess, (state, action)=> {
+    return {
+        ...state,
+        user:action.user,
+    }
+})
+)
 
 
 export function AuthReducer(state:any,action:any){
