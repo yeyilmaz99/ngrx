cf9bf262329b69b69cf4c608e3af5ba25bc99789
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-03-20 21:13:41 +0300

    signup

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/store/app.state';
+import { setLoadingSpinner } from 'src/app/store/shared/shared.actions';
+import { signupStart } from '../state/auth.actions';
 
 @Component({
   selector: 'app-signup',
@@ -10,7 +14,7 @@ export class SignupComponent implements OnInit {
 
   signUpForm:FormGroup;
 
-  constructor() { }
+  constructor(private store:Store<AppState>) { }
 
   ngOnInit(): void {
     this.signUpForm = new FormGroup({
@@ -22,7 +26,10 @@ export class SignupComponent implements OnInit {
   onSignUpSubmit(){
     if(!this.signUpForm.valid){return;}
 
-    
+    const email = this.signUpForm.value.email;
+    const password = this.signUpForm.value.password;
+    this.store.dispatch(setLoadingSpinner({status:true}))
+    this.store.dispatch(signupStart({email,password}))
   }
 
 }
