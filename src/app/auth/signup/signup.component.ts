9f8b1bf6f4e86664ff75229b05a9352935a4b02a
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-03-20 21:02:37 +0300

    signup

---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  signUpForm:FormGroup;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.signUpForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required])
+    })
+  }
+
+  onSignUpSubmit(){
+    if(!this.signUpForm.valid){return;}
+
+    
+  }
+
+}
