0631624c32efcacec09893466b360eb699690f71
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-03-22 23:00:00 +0300

    autoLogin & autoLogout

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -3,6 +3,7 @@ import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { AppState } from 'src/app/store/app.state';
 import {isAuthenticated} from 'src/app/auth/state/auth.selector'
+import { autoLogout } from 'src/app/auth/state/auth.actions';
 
 @Component({
   selector: 'app-header',
@@ -15,6 +16,12 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit(): void {
     this.isAuthenticated = this.store.select(isAuthenticated);
+  }
+
+
+  onLogout(event:Event){
+    event.preventDefault();
+    this.store.dispatch(autoLogout())
   }
 
 }
