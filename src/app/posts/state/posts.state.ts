d8aff6debc1dbfbabc26e4e57f2f7dd14ee322ce
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-03-28 01:18:38 +0300

    getposts

--- src/app/posts/state/posts.state.ts
+++ src/app/posts/state/posts.state.ts
@@ -6,8 +6,5 @@ export interface PostsState {
 }
 
 export const initialState: PostsState = {
-    posts: [
-        {id:'1', title:'sample title 1', description: 'Sample Description 1'},
-        {id:'2', title:'sample title 2', description: 'Sample Description 2'}
-    ]
+    posts: null
 }
