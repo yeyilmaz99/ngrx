a06fc02bb1ff347b40776baea1b4608cc674476d
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-03-30 00:03:09 +0300

    httpinterceptor

--- src/app/services/AuthTokenInterceptor.ts
+++ src/app/services/AuthTokenInterceptor.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { exhaustMap, Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AppState } from '../store/app.state';
+import { getToken } from '../auth/state/auth.selector';
 
 @Injectable()
 export class AuthTokenInterceptor implements HttpInterceptor {
+    constructor(private store:Store<AppState>){}
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        return next.handle(req);
+        return this.store.select(getToken).pipe(exhaustMap(token => {
+            if (!token) {
+                return next.handle(req);
+            }
+            let modifiedReq = req.clone({params: req.params.append('auth', token),
+            });
+            return next.handle(modifiedReq);
+        }))
+
     }
 }
