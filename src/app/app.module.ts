db1178c772db4a7a6a29a640338b8425c913eb2c
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-03-30 00:04:05 +0300

    httpinterceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,10 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from 'src/environments/environment';
 import { appReducer } from './store/app.state';
 import { EffectsModule } from '@ngrx/effects';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { LoadingSpinnerComponent } from './shared/components/loading-spinner/loading-spinner.component'
 import { AuthEffects } from './auth/state/auth.effects';
+import { AuthTokenInterceptor } from './services/AuthTokenInterceptor';
 
 @NgModule({
   declarations: [
@@ -34,7 +35,7 @@ import { AuthEffects } from './auth/state/auth.effects';
     FormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass:AuthTokenInterceptor, multi:true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
