d989ead20d57996028fa967b1a1db6bf69e4183b
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-03-30 00:31:33 +0300

    auth guard

---
+++ src/app/services/auth.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { exhaustMap, map, Observable } from 'rxjs';
+import { isAuthenticated } from '../auth/state/auth.selector';
+import { AppState } from '../store/app.state';
+
+@Injectable({providedIn: 'root'})
+export class AuthGuard implements CanActivate {
+    constructor(private store:Store<AppState> , private router:Router) { }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):boolean  | UrlTree  | Observable<boolean | UrlTree> | Promise<boolean | UrlTree>{
+        return this.store.select(isAuthenticated).pipe(map((authenticate) => {
+            if(!authenticate) {
+                return this.router.createUrlTree(['auth']);
+            }
+            return true;
+        }))
+    }
+}
