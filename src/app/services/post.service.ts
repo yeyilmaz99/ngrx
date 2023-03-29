46dd0a09c858add55b6db2b138688ff14b1ee951
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-03-29 23:25:56 +0300

    update & delete

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -23,4 +23,13 @@ export class PostsService {
     addPost(post:Post):Observable<{name:string}>{
         return this.http.post<{name:string}>('https://vue-completecourse.firebaseio.com/posts.json',post)
     }
+
+    updatePost(post:Post){
+        const postData = {[post.id]: {title:post.title, description:post.description}}
+        return this.http.patch('https://vue-completecourse.firebaseio.com/posts.json',postData)
+    }
+
+    deletePost(id:string){
+        return this.http.delete(`https://vue-completecourse.firebaseio.com/posts/${id}.json`)
+    }
 }
