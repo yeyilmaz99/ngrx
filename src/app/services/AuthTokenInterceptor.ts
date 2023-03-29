76642bbbbc917cc4094aefc69cbda7fc944cd63c
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-03-30 00:41:53 +0300

    debug

--- src/app/services/AuthTokenInterceptor.ts
+++ src/app/services/AuthTokenInterceptor.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest } from '@angular/common/http';
-import { exhaustMap, Observable } from 'rxjs';
+import { exhaustMap, Observable, take } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { AppState } from '../store/app.state';
 import { getToken } from '../auth/state/auth.selector';
@@ -9,7 +9,7 @@ import { getToken } from '../auth/state/auth.selector';
 export class AuthTokenInterceptor implements HttpInterceptor {
     constructor(private store:Store<AppState>){}
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        return this.store.select(getToken).pipe(exhaustMap(token => {
+        return this.store.select(getToken).pipe(take(1),exhaustMap(token => {
             if (!token) {
                 return next.handle(req);
             }
