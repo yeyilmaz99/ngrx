cc30329204b9a81ab24964d78985455a9a17db98
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-03-31 02:10:09 +0300

    routerState

--- src/app/posts/state/posts.selector.ts
+++ src/app/posts/state/posts.selector.ts
@@ -1,4 +1,7 @@
+import { RouterState } from "@ngrx/router-store";
 import { createFeatureSelector, createSelector } from "@ngrx/store";
+import { RouterStateUrl } from "src/app/router/custom-serializer";
+import { getCurrentRoute } from "src/app/router/router.selector";
 import { PostsState } from "./posts.state";
 
 export const POST_STATE_NAME = 'posts'
@@ -9,6 +12,6 @@ export const getPosts = createSelector(getPostsState, (state) => {
     return state.posts;
 })
 
-export const getPostById = createSelector(getPostsState, (state:any, props:any) => {
-    return state.posts.find((post:any) => post.id === props.id)
+export const getPostById = createSelector(getPosts, getCurrentRoute, (posts:any, route:RouterStateUrl) => {
+    return posts ? posts.find((post:any) => post.id === route.params['id']) : null;
 })
