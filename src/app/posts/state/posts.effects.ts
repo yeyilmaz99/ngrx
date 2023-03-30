66db7112d1bf23e5030e0705c122825ff397ebf8
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-03-31 02:09:52 +0300

    updateRedirect

--- src/app/posts/state/posts.effects.ts
+++ src/app/posts/state/posts.effects.ts
@@ -1,12 +1,16 @@
 import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
-import { map, mergeMap, switchMap } from "rxjs";
+import { Store } from "@ngrx/store";
+import { map, mergeMap, switchMap, tap } from "rxjs";
 import { PostsService } from "src/app/services/post.service";
+import { AppState } from "src/app/store/app.state";
+import { setErrorMessage } from "src/app/store/shared/shared.actions";
 import { addPost, addPostSuccess, deletePost, deletePostSuccess, loadPosts, loadPostsSuccess, updatePost, updatePostSuccess } from "./posts.actions";
 
 @Injectable()
 export class PostsEffects {
-    constructor(private actions$: Actions, private postsService:PostsService){
+    constructor(private actions$: Actions, private postsService:PostsService, private store:Store<AppState>, private router:Router){
 
     }
 
@@ -31,7 +35,8 @@ export class PostsEffects {
     updatePost$ = createEffect (() => {
         return this.actions$.pipe(ofType(updatePost), switchMap((action)=> {
             return this.postsService.updatePost(action.post).pipe(map((data) => {
-                return updatePostSuccess({post:action.post})
+                return updatePostSuccess({post:action.post, redirect:true})
+
             }))
         }))
     })
@@ -43,4 +48,25 @@ export class PostsEffects {
             }))
         }))
     })
+
+
+    updateRedirect$ = createEffect(
+        () => {
+          return this.actions$.pipe(
+            ofType(...[updatePostSuccess]),
+            tap((action)=> {
+              this.store.dispatch(setErrorMessage({message:''}))
+              if(action.redirect) {
+                this.router.navigate(['/posts']);
+              }
+            })
+          )
+        },
+        {dispatch: false}
+      )
+
+    
+
+
+
 }
