2b8504d1b3fa12092c7b82cbc69628e55c3e1fc2
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-03-31 03:15:12 +0300

    singlePost effects

--- src/app/posts/state/posts.effects.ts
+++ src/app/posts/state/posts.effects.ts
@@ -1,8 +1,14 @@
 import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
+import {
+    RouterNavigatedAction,
+    routerNavigationAction,
+    ROUTER_NAVIGATION,
+  } from '@ngrx/router-store';
+
 import { Store } from "@ngrx/store";
-import { map, mergeMap, switchMap, tap } from "rxjs";
+import { filter, map, mergeMap, switchMap, tap } from "rxjs";
 import { PostsService } from "src/app/services/post.service";
 import { AppState } from "src/app/store/app.state";
 import { setErrorMessage } from "src/app/store/shared/shared.actions";
@@ -10,14 +16,14 @@ import { addPost, addPostSuccess, deletePost, deletePostSuccess, loadPosts, load
 
 @Injectable()
 export class PostsEffects {
-    constructor(private actions$: Actions, private postsService:PostsService, private store:Store<AppState>, private router:Router){
+    constructor(private actions$: Actions, private postsService: PostsService, private store: Store<AppState>, private router: Router) {
 
     }
 
     loadPost$ = createEffect(() => {
         return this.actions$.pipe(ofType(loadPosts), mergeMap((action) => {
             return this.postsService.getPosts().pipe(map((posts) => {
-                return loadPostsSuccess({posts})
+                return loadPostsSuccess({ posts })
             }))
         }))
     })
@@ -26,25 +32,25 @@ export class PostsEffects {
     addPost$ = createEffect(() => {
         return this.actions$.pipe(ofType(addPost), mergeMap(action => {
             return this.postsService.addPost(action.post).pipe(map(data => {
-                const post = {...action.post, id:data.name};
-                return addPostSuccess({post});
+                const post = { ...action.post, id: data.name };
+                return addPostSuccess({ post });
             }))
         }))
     },)
 
-    updatePost$ = createEffect (() => {
-        return this.actions$.pipe(ofType(updatePost), switchMap((action)=> {
+    updatePost$ = createEffect(() => {
+        return this.actions$.pipe(ofType(updatePost), switchMap((action) => {
             return this.postsService.updatePost(action.post).pipe(map((data) => {
-                return updatePostSuccess({post:action.post, redirect:true})
+                return updatePostSuccess({ post: action.post, redirect: true })
 
             }))
         }))
     })
 
-    deletePost$ = createEffect (() => {
-        return this.actions$.pipe(ofType(deletePost), switchMap((action)=> {
+    deletePost$ = createEffect(() => {
+        return this.actions$.pipe(ofType(deletePost), switchMap((action) => {
             return this.postsService.deletePost(action.id).pipe(map((data) => {
-                return deletePostSuccess({id:action.id})
+                return deletePostSuccess({ id: action.id })
             }))
         }))
     })
@@ -52,20 +58,34 @@ export class PostsEffects {
 
     updateRedirect$ = createEffect(
         () => {
-          return this.actions$.pipe(
-            ofType(...[updatePostSuccess]),
-            tap((action)=> {
-              this.store.dispatch(setErrorMessage({message:''}))
-              if(action.redirect) {
-                this.router.navigate(['/posts']);
-              }
-            })
-          )
+            return this.actions$.pipe(
+                ofType(...[updatePostSuccess]),
+                tap((action) => {
+                    this.store.dispatch(setErrorMessage({ message: '' }))
+                    if (action.redirect) {
+                        this.router.navigate(['/posts']);
+                    }
+                })
+            )
         },
-        {dispatch: false}
-      )
+        { dispatch: false }
+    )
 
-    
+    getSinglePost$ = createEffect(() => {
+        return this.actions$.pipe(ofType(ROUTER_NAVIGATION), filter((r: RouterNavigatedAction) => {
+            return r.payload.routerState.url.startsWith('/posts/details')
+        }), map((r: RouterNavigatedAction) => {
+            return r.payload.routerState['params']['id'];
+        }), switchMap((id) => {
+            return this.postsService.getPostById(id).pipe(map((post) => {
+                const postData = [{...post, id}];
+                return loadPostsSuccess({posts:postData})
+            }))
+        })
+        )
+    })
+
+
 
 
 
