60ee66bd178c2b8c14146d8e32a41cb97453d85e
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-03-31 03:13:31 +0300

    singlePost routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { SinglePostComponent } from './posts/single-post/single-post.component';
 import { AuthGuard } from './services/auth.guard';
 
 const routes: Routes = [
@@ -12,6 +13,9 @@ const routes: Routes = [
   },
   {
     path:'posts', loadChildren:() => import('./posts/posts.module').then(m => m.PostsModule),canActivate:[AuthGuard]
+  },
+  {
+    path:'posts/details/:id', component:SinglePostComponent
   },
   {
     path:'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
