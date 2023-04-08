bf9d63d1eb31ef0ec05cd0001a138fee858a9a72
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-04-08 04:46:27 +0300

    ngrx entity crud

--- src/app/posts/state/posts.actions.ts
+++ src/app/posts/state/posts.actions.ts
@@ -1,6 +1,6 @@
 import { createAction, props } from "@ngrx/store";
 import { Post } from "src/app/models/post.model";
-
+import {Update} from "@ngrx/entity";
 
 export const ADD_POST_ACTION = '[post page] add post';
 export const ADD_POST_SUCCESS = '[post page] add post success'
@@ -20,7 +20,7 @@ export const addPost = createAction(ADD_POST_ACTION, props<{post:Post}>());
 export const addPostSuccess = createAction(ADD_POST_SUCCESS, props<{post:Post}>())
 
 export const updatePost = createAction(UPDATE_POST_ACTION, props<{post:Post}>())
-export const updatePostSuccess = createAction(UPDATE_POST_SUCCESS, props<{post:Post, redirect:boolean}>())
+export const updatePostSuccess = createAction(UPDATE_POST_SUCCESS, props<{post:Update<Post>, redirect:boolean}>())
 
 export const deletePost = createAction(DELETE_POST_ACTION, props<{id:string}>())
 export const deletePostSuccess = createAction(DELETE_POST_SUCCESS, props<{id:string}>())
