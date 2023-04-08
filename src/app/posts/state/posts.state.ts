b986863c5a186ec9a12dc2259bc4b21a02894bc4
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-04-08 04:46:37 +0300

    ngrx entity crud

--- src/app/posts/state/posts.state.ts
+++ src/app/posts/state/posts.state.ts
@@ -1,10 +1,11 @@
+import { EntityState, createEntityAdapter } from "@ngrx/entity";
 import { Post } from "src/app/models/post.model"
 
 
-export interface PostsState {
-    posts : Post[];
-}
+export interface PostsState extends EntityState<Post> {}
 
-export const initialState: PostsState = {
-    posts: null
-}
+export const postsAdapter = createEntityAdapter<Post>();
+
+export const initialState: PostsState = postsAdapter.getInitialState(); 
+
+
