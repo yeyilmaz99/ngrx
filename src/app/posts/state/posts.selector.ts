3de7ee25cbec19271e019fad5e1765545fca7452
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-04-08 04:58:39 +0300

    ngrx entity crud

--- src/app/posts/state/posts.selector.ts
+++ src/app/posts/state/posts.selector.ts
@@ -2,16 +2,15 @@ import { RouterState } from "@ngrx/router-store";
 import { createFeatureSelector, createSelector } from "@ngrx/store";
 import { RouterStateUrl } from "src/app/router/custom-serializer";
 import { getCurrentRoute } from "src/app/router/router.selector";
-import { PostsState } from "./posts.state";
+import { PostsState, postsAdapter } from "./posts.state";
 
 export const POST_STATE_NAME = 'posts'
 const getPostsState = createFeatureSelector<PostsState>(POST_STATE_NAME);
 
+export const postsSelectors = postsAdapter.getSelectors();
+export const getPosts = createSelector(getPostsState, postsSelectors.selectAll);
+export const getPostEntities = createSelector(getPostsState, postsSelectors.selectEntities);
 
-export const getPosts = createSelector(getPostsState, (state) => {
-    return state.posts;
-})
-
-export const getPostById = createSelector(getPosts, getCurrentRoute, (posts:any, route:RouterStateUrl) => {
-    return posts ? posts.find((post:any) => post.id === route.params['id']) : null;
+export const getPostById = createSelector(getPostEntities, getCurrentRoute, (posts:any, route:RouterStateUrl) => {
+    return posts ? posts[route.params['id']]: null;
 })
