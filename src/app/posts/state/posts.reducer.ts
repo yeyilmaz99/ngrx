f09b00bd6d1965f8b804196e76f215d51aa29d26
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-04-08 04:46:35 +0300

    ngrx entity crud

--- src/app/posts/state/posts.reducer.ts
+++ src/app/posts/state/posts.reducer.ts
@@ -1,41 +1,31 @@
-import { createReducer, on } from "@ngrx/store";
-import { addPostSuccess, deletePost, deletePostSuccess, loadPostsSuccess, updatePost, updatePostSuccess } from "./posts.actions";
-import { initialState } from "./posts.state";
+import { createReducer, on } from '@ngrx/store';
+import {
+  addPostSuccess,
+  deletePost,
+  deletePostSuccess,
+  loadPostsSuccess,
+  updatePost,
+  updatePostSuccess,
+} from './posts.actions';
+import { initialState, postsAdapter } from './posts.state';
 
-const _postReducer = createReducer(initialState,
+const _postReducer = createReducer(
+  initialState,
 
-     on(addPostSuccess, (state,action) =>{
-    let post = {...action.post};
-    return {
-        ...state,
-        posts: [...state.posts, post]
-    }
-}), on(updatePostSuccess, (state, action) => {
-    const updatedPosts = state.posts.map(post => {
-        return action.post.id === post.id ? action.post : post;
-    })
-    return {
-        ...state,
-        posts: updatedPosts
+  on(addPostSuccess, (state, action) => {
+    return postsAdapter.addOne(action.post, state);
+  }),
+  on(updatePostSuccess, (state, action) => {
+    return postsAdapter.updateOne(action.post, state)
+  }),
+  on(deletePostSuccess, (state, action) => {
+    return postsAdapter.removeOne(action.id, state);
+  }),
+  on(loadPostsSuccess, (state, action) => {
+    return postsAdapter.setAll(action.posts, state);
+  })
+);
 
-    }
-}), on(deletePostSuccess, (state, action) => {
-    const updatedPosts = state.posts.filter(post => {
-        return post.id !== action.id;
-    })
-    return {
-        ...state,
-        posts: updatedPosts
-    }
-}),on(loadPostsSuccess, (state, action) => {
-    return {
-        ...state,
-        posts:action.posts
-    }
-})
-)
-
-
-export function postsReducer(state:any, action:any){
-    return _postReducer(state,action);
+export function postsReducer(state: any, action: any) {
+  return _postReducer(state, action);
 }
